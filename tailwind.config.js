60d26866da8d1223e3375d1b88b52382e79b48dc
Bun <bun2932005@gmail.com>
2024-10-26 20:57:15 +0700

    build: add prettier to project

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,10 @@
 module.exports = {
   content: [
     './views/**/*.pug', // Quét tất cả các file .pug trong thư mục views
-    './public/**/*.js',  // Quét các file JS trong thư mục public
+    './public/**/*.js', // Quét các file JS trong thư mục public
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
