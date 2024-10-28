4fea0d3a5c1ed587c41f82b6d0b70a3518b6e664
Bun <bun2932005@gmail.com>
2024-10-28 23:16:36 +0700

    style: product first_banner UI

--- controllers/dashboard.controller.ts
+++ controllers/dashboard.controller.ts
@@ -1,5 +1,13 @@
 import { Request, Response } from 'express'
+import PetModel from '../models/pet.model'
 
+// [GET] pet infomation
 export const index = async (req: Request, res: Response) => {
-  res.render('pages/dashboard/index', {})
-}
+  const petInfo = await PetModel.find({
+    deleted: false
+  });
+
+  res.render('pages/dashboard/index', {
+    petInfo: petInfo
+  });
+};
---
+++ models/pet.model.ts
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const petSchema = new mongoose.Schema(
+    {
+        id_code: String,
+        name: String,
+        gene: String,
+        age: Number,
+        price: Number,
+        avt: String,
+        images: Array,
+        size: String,
+        color: String,
+        vacinated: String,
+        dewormed: String,
+        cert: String,
+        location: String,
+        published_date: Date,
+        additional_info: String,
+        deleted: Boolean
+    },
+    {
+        timestamps: true
+    }
+);
+
+const PetModel = mongoose.model("PetModel", petSchema, "pet");
+
+export default PetModel;
