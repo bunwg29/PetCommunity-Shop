01afc60a7e290be2e67b7a74f1c92f9642532b85
Bun <bun2932005@gmail.com>
2024-10-30 19:20:10 +0700

    style: handle multiple header

--- controllers/dashboard.controller.ts
+++ controllers/dashboard.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express'
 import PetModel from '../models/pet.model'
 import FoodPetModel from '../models/foodPet.model';
 import ToyPetModel from '../models/toyPet.model';
+import BlogModel from '../models/blog.model';
 // [GET] Information about new pet, new product is served for dashboard interface
 export const index = async (req: Request, res: Response) => {
   const find = {
@@ -13,9 +14,13 @@ export const index = async (req: Request, res: Response) => {
 
   const toyPetInfo = await ToyPetModel.find(find);
 
+  const blogInfo = await BlogModel.find(find);
+
   res.render('pages/dashboard/index', {
     petInfo: petInfo,
     foodPetInfo: foodPetInfo,
-    toyPetInfo: toyPetInfo
+    toyPetInfo: toyPetInfo,
+    blogInfo: blogInfo,
+    isDashboard: true
   });
 };
---
+++ models/blog.model.ts
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const blogSchema = new mongoose.Schema(
+    {
+        avt: String,
+        title: String,
+        description: String,
+        content: String,
+        uploadBy: String,
+        deleted: {
+            type: Boolean,
+            default: false
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+const BlogModel = mongoose.model("BlogModel", blogSchema, "blog");
+
+export default BlogModel;
