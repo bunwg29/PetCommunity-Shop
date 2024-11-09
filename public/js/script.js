b79c68fe87668928ebefe5cc42d9efeb685e21f5
Bun <bun2932005@gmail.com>
2024-11-09 16:31:47 +0700

    style: user dropdown bar at the header

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,4 @@
-// Handle hidden signin button and replace by image
+// Start handle hidden signin button and replace by image
 
 const avatarUser = document.querySelector(".avatar-user");
 const buttonHeader = document.querySelector(".button-header");
@@ -8,3 +8,21 @@ if(avatarUser) {
 }
 
 // End handle hidden...
+
+// Start dropdown info user
+
+const dropdownAvatar = document.querySelector(".dropdown-avatar");
+
+avatarUser.addEventListener("click", () => {
+    dropdownAvatar.classList.toggle("hidden");
+});
+
+document.addEventListener("click", (event) => {
+    if (!dropdownAvatar.classList.contains("hidden") &&
+        !dropdownAvatar.contains(event.target) &&
+        event.target !== avatarUser) {
+        dropdownAvatar.classList.add("hidden");
+    }
+});
+
+// End dropdown info user
