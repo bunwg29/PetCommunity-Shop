6d1a7c027d02ac8770d20c2d4ba88531f728e552
Bun <bun2932005@gmail.com>
2024-11-15 00:48:38 +0700

    feat: handle upload multiple images on cloud

--- middlewares/uploadCloud.middleware.ts
+++ middlewares/uploadCloud.middleware.ts
@@ -1,20 +1,43 @@
 import { NextFunction, Request, Response } from 'express'
 import { streamUpload } from '../helpers/streamUpload.helper'
 
-export const uploadSingle = (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
+export const uploadSingle = ( req: Request, res: Response, next: NextFunction) => {
+
   if (req['file']) {
+
     const uploadToCloudinary = async (buffer: any) => {
+
       const result = await streamUpload(buffer)
       req.body[req['file'].fieldname] = result['url']
-      next()
-    }
+      next();
 
-    uploadToCloudinary(req['file'].buffer)
-  } else {
-    next()
-  }
-}
+    } 
+
+    uploadToCloudinary(req['file'].buffer);
+
+  } else { next(); }
+
+};
+
+export const uploadMultiple = async (req: Request, res: Response, next: NextFunction) => {
+
+  if (req.files) {
+    try {
+      
+      const uploadPromises = (req.files as Express.Multer.File[]).map((file) => 
+        streamUpload(file.buffer)
+      );
+
+      const results = await Promise.all(uploadPromises);
+  
+      req.body['images'] = results.map((result) => result['url']);
+  
+      next();
+
+    } catch (error) {
+      res.send("TOANG");
+    }
+  } else { next(); }
+
+};
+
