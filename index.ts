269e4d668eea9b0a1ac1139b55d490d08f9cf99c
Bun <bun2932005@gmail.com>
2024-11-23 13:48:29 +0700

    vendor(dev dependencies): flash and session

--- index.ts
+++ index.ts
@@ -3,17 +3,19 @@ import express, { Express } from 'express';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-import * as database from './config/database';
-
-import Routers from './routes/client/index.route';
-import RoutersAdmin from './routes/admin/index.route';
 import methodOverride from 'method-override';
 import cookieParser from 'cookie-parser';
 import path from 'path';
+import flash from 'express-flash';
+import session, { SessionOptions } from 'express-session';
+
+import * as database from './config/database';
+database.connect();
+
+import Routers from './routes/client/index.route';
+import RoutersAdmin from './routes/admin/index.route';
 
 import { systemConfig } from './config/adminPrefix';
-
-database.connect();
 
 const app: Express = express();
 const port: number | string = process.env.PORT || 3000;
@@ -33,12 +35,20 @@ app.use(
   express.static(path.join(__dirname, 'node_modules', 'tinymce'))
 );
 
-app.use(cookieParser());
+const sessionOptions: SessionOptions = {
+  secret: process.env.SESSION_SECERET,
+  resave: false,
+  saveUninitialized: true,
+  cookie: { maxAge: 60000 },
+};
+
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(session(sessionOptions));
+app.use(flash());
 
 app.locals.prefixAdmin = systemConfig.prefixAdmin;
 
 Routers(app);
-
 RoutersAdmin(app);
 
 app.listen(port, () => {
